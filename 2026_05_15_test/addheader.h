281d6292a455bc8bcd1e3d59fd793c01c86bfe48
fractalnumber <mbchset@gmail.com>
2026-05-26 17:40:13 +0900

    포인터연습

--- 2026_05_15_test/addheader.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-int diceRresult(int* DiceArray, int maxrollcount);
-
-int arrayinv(int* array, int size);
-
-int arraylotto(int* array, int size);
---
+++ 2026_05_15_test/functions.cpp
@@ -0,0 +1,5 @@
+#include <iostream>
+#include <time.h>
+#include <stdio.h>
+#include "functions.h"
+
