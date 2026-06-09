5ecdef3117c61001dd33ede73cd7977b3cab3395
fractalnumber <mbchset@gmail.com>
2026-06-09 17:41:13 +0900

    6월9일

---
+++ 2026_05_15_test/header_0609.cpp
@@ -0,0 +1,92 @@
+#include <iostream>
+#include <conio.h>
+#include <vector>
+#include <windows.h>
+#include "header_0609.h"
+
+void MainQueue()
+{
+	char InputCommand = '0';
+	int QueueSize = 0;		
+	int EntryPoint = -1;
+
+	std::vector<char> QueueArray = {0, };
+	
+	printf("큐 크기를 입력: ");	
+		
+	std::cin >> QueueSize;
+	std::cin.clear();
+	std::cin.ignore(10000,'\n');
+
+	if (QueueSize <= 0)
+	{
+		printf("\nError");
+		return;
+	}
+	
+	QueueArray.reserve(QueueSize);
+	QueueArray.resize(QueueSize);
+
+	printf("\n크기를 지정한 큐에 들어갈 문자 입력: ");
+
+	for (int i = 0; i < QueueSize; i++)
+	{
+		QueueArray[i] = _getch();
+		EntryPoint++;		
+	}
+
+	
+
+	while (InputCommand != '3')
+	{
+		system("cls");
+
+		printf("현재 큐에 들어간 요소는 : \n");
+		for (int i = 0; i < EntryPoint+1; i++)
+		{
+			printf("[%c] ", QueueArray[i]);
+		}
+
+		printf("\n\n큐의 사이즈는 : %d", EntryPoint+1);
+		// printf("\n진입점(확인용) : [%d]", EntryPoint);
+
+		printf("\n\n수행할 작업 입력 : \n\n1. 큐에서 값 빼기\n2. 큐에 값 넣기\n3. 나가기 (.)\n입력 :");
+		std::cin >> InputCommand;
+		std::cin.clear();
+		std::cin.ignore(10000, '\n');
+
+		if (InputCommand == '1') // 큐에서 값 빼기
+		{
+			if (EntryPoint > 0)
+			{
+				for (int i = 0; i < EntryPoint; i++)
+				{
+					QueueArray[i] = QueueArray[i + 1];
+					QueueArray[i + 1] = NULL;
+				}
+				EntryPoint--;
+			}
+			else if (EntryPoint == 0)
+			{
+				QueueArray[0] = NULL;
+				EntryPoint = -1;
+				printf("\n큐에 값이 전부 비워졌습니다.");
+				_getch();
+			}
+			else if (EntryPoint < 0)
+			{
+				printf("\n큐에 값이 하나도 없어 뺄 수 없습니다.");
+				_getch();
+			}
+		}
+		else if (InputCommand == '2') // 큐에 값 넣기
+		{
+			printf("\n큐에 들어갈 값 입력 : ");
+			QueueArray.resize(EntryPoint+2);
+			QueueArray[EntryPoint + 1] = _getch();
+			EntryPoint++;
+		}		
+	}
+
+}
+
--- 2026_05_15_test/outsideworks.cpp
+++ 2026_05_15_test/outsideworks.cpp
@@ -7,9 +7,11 @@
 #include <stdio.h>
 #include <time.h>
 #include <fstream>
-#include <vector>
+// #include <vector>
 #include <random>
-#include "mazegame.h"
+#include <conio.h>
+#include <stack>
+#include "header_0609.h"
 
 using namespace std;
 
@@ -22,43 +24,61 @@ int main()
 	// TestVarious();
 	// mazegame();
 
-	std::vector<int> Array;
-
-	Array.push_back(5);
-	Array.push_back(6);
-	Array.push_back(7);
-	Array.push_back(8);
-	Array.push_back(9);
-	Array.push_back(10);
-
+	// std::stack<char> Array;
 	
+	// printf("7개의 값을 입력합니다.");
+	 
+	// Array.push(_getch());
+	// Array.push(_getch());
+	// Array.push(_getch());
+	// Array.push(_getch());
+	// Array.push(_getch());
+	// Array.push(_getch());
+	// Array.push(_getch());
+	
+	// printf("스택의 현재 크기는[%llu] 입니다.", Array.size());
+	// printf("\n\n현재 스택의 마지막 값은 [%c]", Array.top());
+ 
+	// Array.pop();
+	
+	// printf("\n\npop을 한번 실행한 스택의 마지막 값은 [%c]", Array.top());
+	
+	// 스택에서 역순출력 할 때는 출력후 (size-1)번 만큼 pop을 하는 것을 반복한다. 데이터를 보존해야 한다면 pop 전에 다른 배열이라던지 보관한다.
 
-	for (int i = 0; i < Array.size(); i++) // Array의 마지막까지 출력?
-	{
-		printf("[%d] ", Array[i]);
-	}
+
+	// for (int i = Array.size() - 1; i >= 0; i--) // Array의 첫번째까지 출력? 첫번째 값을 확인하고 싶으면 *.size() 에서 -1 을 빼서 확인하면 된다.
+	// {
+	// 	printf("[%c] ", Array[i]);
+	// }
+
+	// printf("\n\n위 부분은 역순 출력됨");
 	
 	// 이하 테스트중
 	
-	 printf("\n현재 vector의 capacity 크기는 [%llu]", Array.capacity());
+	// printf("\n현재 vector의 capacity 크기는 [%llu]", Array.capacity());
 	
-	 Array.reserve(10);
+	// Array.reserve(10);
 
-	 printf("\n현재 vector의 크기는 [%llu]", Array.capacity());
+	// printf("\n현재 vector의 크기는 [%llu]", Array.capacity());
 
 	// printf("\n Array의 특정 주소의 실제 값은 : %d", Array[6]); // reserve를 통해 메모리 공간 할당은 받았으나 실제 값이 들어가기 전까지 해당 주소로 접근은 불가능함.
 
-	 Array.pop_back();
+	// Array.pop();
 	
 
 
-	 printf("\npop_back을 한번 실행한 현재 vector의 size 값은 [%llu]", Array.size());
-	 printf("\npop_back을 한번 실행한 현재 vector의 마지막 주소의 값은 [%d]", Array[Array.size()-1]);
+	// printf("\n\npop_back을 한번 실행한 현재 vector의 size 값은 [%llu]", Array.size());
+	// printf("\n\npop_back을 한번 실행한 현재 vector의 마지막 주소의 값은 [%c]", Array[Array.size()-1]);
 
-	 Array.insert(Array.begin(), 1);
+	// Array.insert(Array.begin(), 'X');
 	 
-	 printf("\n%d", Array[0]);
-	
+	// printf("\n\nInsert Test입니다. Array 맨 처음에 들어간 값은 %c", Array[0]);
+
+
+
+	MainQueue();
+
+
 }
 
  
